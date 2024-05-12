0e594598618b60d353402d9f4dc42756b43ddbaa
Vicky <mvictoria_29397@hotmail.es>
2024-05-12 16:03:57 +0200

    create saga controller

--- src/main/java/com/mca/controller/SagaController.java
+++ src/main/java/com/mca/controller/SagaController.java
@@ -1,21 +1,16 @@
 package com.mca.controller;
 
-import com.mca.data.mapper.GameMapper;
-import com.mca.data.repository.GameRepository;
-import com.mca.infrastructure.model.videoGame.Game;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 public class SagaController implements GameSagaApiDelegate{
 
+    // Where are the relates games?
     @Override
     public ResponseEntity<Set<String>> getGameSagaRelatedSagas(String gameId) {
-        return GameSagaApiDelegate.super.getGameSagaRelatedSagas(gameId);
+        return null;
     }
 }
