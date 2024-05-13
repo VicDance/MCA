69d987290b4566222b05ddad562ac51b8b3d8944
Vicky <mvictoria_29397@hotmail.es>
2024-05-13 18:19:26 +0200

    add base packages

--- src/main/java/com/mca/McaApplication.java
+++ src/main/java/com/mca/McaApplication.java
@@ -2,9 +2,11 @@ package com.mca;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.mca.data.service", "com.mca.controller", "com.mca.service"})
 public class McaApplication {
 
 	public static void main(String[] args) {
