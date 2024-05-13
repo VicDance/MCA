92a68e9d15becfa9f0197891b1eeef3a10b46690
Vicky <mvictoria_29397@hotmail.es>
2024-05-13 18:56:09 +0200

    add instance to mapper

--- src/main/java/com/mca/data/mapper/GameMapper.java
+++ src/main/java/com/mca/data/mapper/GameMapper.java
@@ -4,9 +4,12 @@ import com.mca.infrastructure.model.VideoGameEntity;
 import com.mca.infrastructure.model.videoGame.Game;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface GameMapper {
+
+    GameMapper INSTANCE = Mappers.getMapper(GameMapper.class);
 
     @Mapping(target = "id", source = "entity.id")
     @Mapping(target = "title", source = "entity.name")
